f1417b5c06640dcad600300c8bb0571646db1198
Yang <stevenyang1108@gmail.com>
2015-07-14 12:41:52 +0800

    Initial commit

---
+++ app/src/main/java/com/example/yang/mycalculator/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.yang.mycalculator;
+
+import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+
+public class MainActivity extends Activity {
+
+    private String express;
+    private double answer;
+    private TextView expArea;
+    private TextView ansArea;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        express = "";
+        answer = 0.0;
+        expArea = (TextView)findViewById(R.id.expArea);
+        ansArea = (TextView)findViewById(R.id.ansArea);
+
+    }
+
+    public void btnPressed(View view)
+    {
+        express += view.getTag().toString();
+        expArea.setText(express);
+    }
+    public void backSpace(View view)
+    {
+        if (!express.equals(""))
+            express = express.substring(0, express.length() - 2);
+    }
+    public void computAction(View view)
+    {
+
+    }
+
+
+}
